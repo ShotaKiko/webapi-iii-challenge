166aac8e1b9b1b9f850a30c2b2799c42585fec2d
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-05-18 10:37:24 -0500

    initial commit/added custom logger middleware

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code away!
+const server = require('./server.js')
+
+
+server.listen(6492, () => {
+    console.log('hello world')
+}) 
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = 'express';
 
+const express = require('express')
 const server = express();
+const timeStamp = new Date()
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +11,10 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`${req.method} type, ${req.url} url, ${timeStamp} timestamp  `)
+  next()
 };
+
+
 
 module.exports = server;
